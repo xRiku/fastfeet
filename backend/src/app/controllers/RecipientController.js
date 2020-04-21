dcb3e0dbd465cd9d0068d509f2237af127437d24
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-04-21 09:38:57 -0300

    Middleware de autenticação feito

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -1,9 +1,13 @@
 const Recipient = require('../models/Recipient');
 
 class RecipientController {
-  async store(req, res){
+  async store(req, res) {
     const recipient = await Recipient.create(req.body);
     return res.json(recipient);
+  }
+
+  async update(req, res) {
+    return res.json({text: 'ok'});
   }
 }
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const SessionController = require('./app/controllers/SessionController')
 const RecipientController = require('./app/controllers/RecipientController')
-
+const authMiddlware = require('./app/middlewares/auth');
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+routes.use(authMiddlware);
 routes.post('/recipients', RecipientController.store);
+routes.put('/recipients', RecipientController.update);
 
 module.exports = routes;
