713dc266362c79d7570c5f7bd67b25cae51ae349
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-05-03 21:52:59 -0300

    Adicionado config de multer e Model de files

---
+++ 2/backend/src/app/models/File.js
@@ -0,0 +1,24 @@
+const { Sequelize, Model } = require('sequelize');
+
+class File extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        filename: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.filename}`;
+          },
+        },
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+}
+
+module.exports = File;
