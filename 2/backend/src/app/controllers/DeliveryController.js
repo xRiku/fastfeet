ee7119c66368a1a0b45bd6bcb4ba727ccd648135
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-05-07 07:59:02 -0300

    Adicionado 2 verificações a controller de encomendas A primeira é se o destinatário existe A segunda é se o entregador existe

--- 2/backend/src/app/controllers/DeliveryController.js
+++ 2/backend/src/app/controllers/DeliveryController.js
@@ -1,7 +1,20 @@
 const Delivery = require("../models/Delivery");
+const Recipient = require("../models/Recipient");
+const Deliveryman = require("../models/Deliveryman");
 
 class DeliveryController {
   async store(req, res) {
+    const { recipient_id, deliveryman_id } = req.body;
+    const checkRecipientExists = await Recipient.findByPk(recipient_id);
+    const checkDeliverymanExists = await Deliveryman.findByPk(deliveryman_id);
+    if (!checkRecipientExists) {
+      return res.status(400).json({ error: 'Recipient does not exist' });
+    }
+
+    if (!checkDeliverymanExists) {
+      return res.status(400).json({ error: 'Deliveryman does not exist' });
+    }
+
     const order = await Delivery.create(req.body);
     return res.json(order);
   }
