7a3ddbbe56d5c32ef9e0214b3647a37bd0316ce4
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-05-07 23:43:07 -0300

    Feito alguma coisa pra ver se vai dar bom dps

--- 2/backend/src/app/controllers/OrderController.js
+++ 2/backend/src/app/controllers/OrderController.js
@@ -1,14 +1,14 @@
 const Deliveryman = require('../models/Deliveryman');
 const Delivery = require('../models/Delivery');
 
-class OrderController{
+class OrderController {
   async index(req, res) {
     const { id } = req.params;
     const orders = await Deliveryman.findOne({
-      where: { 
+      where: {
         id,
       },
-      include: [ {
+      include: [{
         model: Delivery,
         as: 'deliveries',
         where: {
@@ -18,6 +18,17 @@ class OrderController{
     });
     return res.json(orders);
   }
-} 
+
+  async update(req, res) {
+    const { id, order_id } = req.params;
+    const order = await Delivery.findByPk(order_id);
+    const { start_date } = req.body;
+    if (!order) {
+      return res.status(400).json({ error: 'Order is not available' });
+    }
+
+
+  }
+}
 
 module.exports = new OrderController();
